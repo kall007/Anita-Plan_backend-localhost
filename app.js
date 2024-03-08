79971d525cf928d10cc80218ba57c68f407c1472
Rui Duram <ruiduran.jr@gmail.com>
2024-03-08 14:18:56 +0100

    bug fixes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 const Plans = require("./routes/plan.routes");
 app.use("/", Plans);
--- models/User.model.js
+++ models/User.model.js
@@ -5,18 +5,18 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, "Email is required."],
+      required: true,
       unique: true,
       lowercase: true,
       trim: true,
     },
     password: {
       type: String,
-      required: [true, "Password is required."],
+      required: true,
     },
     name: {
       type: String,
-      required: [true, "Name is required."],
+      required: true,
     },
   },
   {
