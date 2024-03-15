5ed9bcbdd00604ab996bdef5181d84e2f105eb49
Kosta Kallias <costakallias03@gmail.com>
2024-03-15 12:02:09 +0100

    cors url change

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL =  "https://master--anita-plan.netlify.app/";
+const FRONTEND_URL =  "https://master--anita-plan.netlify.app";
 // process.env.ORIGIN ||
 
 // Middleware configuration
